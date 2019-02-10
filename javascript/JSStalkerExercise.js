57c63a6aea73fcf1bdd7fa6e5ed09b92f57931d5
williamsdan94 <williamsdan94@gmail.com>
2019-02-10 04:29:31 -0700

    committed

--- javascript/JSStalkerExercise.js
+++ javascript/JSStalkerExercise.js
@@ -1,6 +1,7 @@
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
 var age = prompt("What is your age?");
+var dayAge = age * 365;
 
-console.log("Your full name is " + firstName + " " + lastName);
-console.log("You are " + age +" years old.");
+alert(age + " years is roughly " + dayAge + " days.");
+
+
+
---
+++ javascript/ageExercise.js
@@ -0,0 +1,23 @@
+var age = prompt("What is your age?");
+var odd = age % 2;
+
+if (age < 0) {
+    alert("Age is invalid.");
+}
+
+else if (age < 18) {
+    console.log("You are not allowed at this venue.");
+}
+
+else if (age = 21) {
+    console.log("Happy Birthday yah ANIMAL! Enter this venue and consume all the alcohol you want.");
+}
+
+else if (age < 21) {
+    console.log("You may enter this venue and partake in alcohol consumption.");
+}
+
+else if ( odd = 1) {
+    console.log("Your age is odd!");
+}
+
